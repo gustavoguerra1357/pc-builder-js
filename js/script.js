2ac70aecf57fb4a1685a3c5e6eb4d2fa99b34dd4
gustvo-guerra <gustavoguerra1357@gmail.com>
2026-01-19 17:11:52 -0300

    adding remaining components

--- js/script.js
+++ js/script.js
@@ -41,7 +41,50 @@ const components = {
         { name: "Placa de Vídeo Palit GeForce RTX 5060 Infinity 3 OC NVIDIA", price: 2399.99, image: "https://images0.kabum.com.br/produtos/fotos/776930/placa-de-video-palit-geforce-rtx-5060-infinity-3-oc-nvidia-geforce-8gb-gddr7-128bits-fp4-e-dlss-4-ray-tracing-ne75060t19p1-gb2063s_1747393915_gg.jpg" },
         { name: "Placa De Vídeo Nvidia Geforce Msi RTX5060ti Ventus", price: 5508.00, image: "https://images8.kabum.com.br/produtos/fotos/sync_mirakl/986938/xlarge/Placa-De-V-deo-Nvidia-Geforce-Msi-RTX5060ti-Ventus-Oc-Black-Plus-2x-16gb-128-Bits-HDMI-3x-Dp-912-v535_1768414132.jpg" },
 
-    ]
+    ],
+    motherBoard: [
+        { name: "Placa-Mãe MSI A520M-A PRO", price: 369.99 ,image: "https://images0.kabum.com.br/produtos/fotos/280890/placa-mae-msi-a520m-a-pro-amd-am4-matx-ddr4_1722881948_gg.jpg" },
+        { name: "Placa-Mãe Gigabyte B550M Aorus", price: 950,image: "https://images1.kabum.com.br/produtos/fotos/114781/placa-mae-gigabyte-b550m-aorus-elite-amd-am4-micro-atx-ddr4_1594908595_gg.jpg" },
+        { name: "Placa-Mãe ASRock B450M-HDV", price: 459,image: "https://images7.kabum.com.br/produtos/fotos/111107/placa-mae-asrock-b450m-hdv-r4-0-amd-am4-micro-atx-ddr4-_1590689801_gg.jpg" },
+        { name: "Placa-Mãe ASUS TUF GAMING A520M-PLUS II", price: 549,image: "https://images3.kabum.com.br/produtos/fotos/165133/placa-mae-asus-tuf-gaming-a520m-plus-ii-amd-am4-matx-ddr4-preto-90mb17g0-m0eay0_1736968906_gg.jpg" },
+        { name: "Placa-Mãe MSI B550M Pro-VDH", price: 649,image: "https://images8.kabum.com.br/produtos/fotos/114338/placa-mae-msi-b550m-pro-vdh-wifi-amd-am4-matx_1594996441_gg.jpg" },
+        { name: "Placa Mãe Gigabyte B550M", price: 699.99,image: "https://images2.kabum.com.br/produtos/fotos/875852/placa-mae-gigabyte-b550m-ds3h-ac-r2-amd-am4-micro-atx-ddr4-rgb-wi-fi-6-bluetooth-preto-b550m-ds3h-ac-r2_1751913498_gg.jpg" },
+        { name: "Placa-Mãe MSI MPG B550 Gaming Plus", price: 909,image: "https://images5.kabum.com.br/produtos/fotos/114335/placa-mae-msi-mpg-b550-gaming-plus-amd-am4-atx_1594999681_gg.jpg" },
+        { name: "Placa-Mãe ASUS TUF Gaming B550M-Plus", price: 844,image: "https://images6.kabum.com.br/produtos/fotos/115216/placa-mae-asus-tuf-gaming-b550m-plus-amd-am4-matx-ddr4_1724074976_gg.jpg" },
+        { name: "Placa-Mãe MSI Pro B760M-E", price: 599,image: "https://images9.kabum.com.br/produtos/fotos/524349/placa-mae-msi-pro-b760m-e-intel-lga-1700-m-atx-dd5-pro-b760m-e-ddr4_1719945686_gg.jpg" },
+        { name: "Placa-Mãe Gigabyte A520M K V2", price: 389,image: "https://images1.kabum.com.br/produtos/fotos/448251/placa-mae-gigabyte-a520m-k-v2-amd-micro-atx-ddr4-a520m-k-v2_1689086164_gg.jpg" },
+        { name: "Placa-Mãe Asus TUF Gaming B550M-PLUS", price: 806,image: "https://images6.kabum.com.br/produtos/fotos/sync_mirakl/172706/xlarge/Placa-M-e-Asus-TUF-Gaming-B550M-PLUS-AMD-AM4-mATX-DDR4-M-2-Aura-para-fita-RGB-90MB14A0-C1BAY0_1768323224.jpg" },
+        { name: "Placa Mãe Gigabyte H610M K DDR4", price: 490,image: "https://images8.kabum.com.br/produtos/fotos/636798/placa-mae-gigabyte-h610m-k-ddr4-intel-lga-1700-micro-atx-ddr4-hdmi-m-2-9mh610mk-00-20_1750790020_gg.jpg" },
+    ],
+    ram: [
+        { name: "Memória RAM Kingston Fury Beast, 16GB", price: 1624 ,image: "https://images6.kabum.com.br/produtos/fotos/172366/memoria-kingston-fury-beast-16gb-3200mhz-ddr4-cl16-preto-kf432c16bb1-16_1626271100_gg.jpg" },
+        { name: "Memória RAM Husky Impulse, 16GB", price: 700 ,image: "https://images6.kabum.com.br/produtos/fotos/922166/memoria-ram-husky-impulse-16gb-3200mhz-ddr4-cl22-preto-hrm001163222pt_1763123037_gg.jpg" },
+        { name: "Memória RAM XPG Gammix D35, 8GB", price: 781 ,image: "https://images9.kabum.com.br/produtos/fotos/474939/memoria-adata-xpg-gammix-d35-8gb-3200mhz-ddr4-cl16-preto-ax4u32008g16a-sbkd35-_1692884059_gg.jpg" },
+        { name: "Memória RAM XPG Spectrix D35G, RGB, 16GB", price: 1506 ,image: "https://images0.kabum.com.br/produtos/fotos/477150/memoria-xpg-spectrix-d35g-rgb-16gb-3200mhz-ddr4-cl16-branco-ax4u320016g16a-swhd35g_1695153931_gg.jpg" },
+        { name: "Memória RAM XPG Lancer Blade, 16GB", price: 2631 ,image: "https://images3.kabum.com.br/produtos/fotos/677473/memoria-ram-xpg-lancer-blade-16gb-rgb-ddr5-6000mhz-single-tray-cl48-preto-ax5u6000c4816g-slabrbk_1736339212_gg.jpg" },
+        { name: "Memória RAM XPG Gammix D35, 16GB", price: 1497 ,image: "https://images1.kabum.com.br/produtos/fotos/474941/memoria-adata-xpg-gammix-d35-16gb-3200mhz-ddr4-cl16-preto-ax4u320016g16a-sbkd35_1692885220_gg.jpg" },
+        { name: "Memória RAM XPG Lancer Blade, 16GB, RGB, DDR5, 6000MHz", price: 2607 ,image: "https://images5.kabum.com.br/produtos/fotos/677475/memoria-ram-xpg-lancer-blade-16gb-rgb-ddr5-6000mhz-single-tray-cl48-branco-ax5u6000c4816g-slabrwh_1736339427_gg.jpg" },
+        { name: "Memória RAM XPG Spectrix D50 RGB, 8GB, 3200MHz", price: 841 ,image: "https://images8.kabum.com.br/produtos/fotos/156588/memoria-xpg-gammix-d50-8gb-3200mhz-ddr4-rgb-cl-16-cinza-ax4u32008g16a-st50_1626985306_gg.jpg" },
+       ],
+    ssd: [
+        { name: "SSD Kingston NV3, 1 TB, M.2 2280", price:  919,image: "https://images2.kabum.com.br/produtos/fotos/621162/ssd-pcie-kingston-nv3-1-tb-m-2-2280-nvme-leitura-6000-mb-s-e-gravacao-4000-mb-s-snv3s-1000g_1726082185_gg.jpg" },
+        { name: "SSD Kingston A400, 240GB, SATA III, 2.5", price:  399,image: "https://images7.kabum.com.br/produtos/fotos/85197/85197_1484306076_gg.jpg" },
+        { name: "SSD Adata SU650, SATA, 240GB, 2.5", price:  259,image: "https://images5.kabum.com.br/produtos/fotos/105015/ssd-sata-adata-su650-240gb-2-5-leitura-520mb-s-e-gravacao-450mb-s-preto-asu650ss-240gt-r_1724166980_gg.jpg" },
+        { name: "SSD Adata Legend 710, 512GB, M.2 2280", price:  499,image: "https://images5.kabum.com.br/produtos/fotos/415885/ssd-adata-legend-710-512gb-m-2-2280-pcie-gen3x4-nvme-1-4-leitura-2-400-mb-s-e-gravacao-1-800-mb-s-azul-aleg-710-512gcs_1675085973_gg.jpg" },
+        { name: "SSD Lexar NS100, 128GB, SATA III", price:  129,image: "https://images7.kabum.com.br/produtos/fotos/102487/ssd-lexar-ns100-128gb-sata-leitura-520mb-s-lns100-128rbna_ssd-lexar-ns100-128gb-sata-leitura-520mb-s-lns100-128rbna_1563205692_gg.jpg" },
+        { name: "SSD Adata Legend 860, 1TB, M.2", price:  919,image: "https://images2.kabum.com.br/produtos/fotos/703112/ssd-adata-legend-860-1tb-m-2-2280-pcie-gen-4x4-nvme-leitura-6000mb-s-gravacao-5000mb-s-compativel-com-ps5-preto-sleg-860-1000gcs_1742820679_gg.jpg" },
+        { name: "SSD 500gb M.2 Nvme PCie 4", price:  684,image: "https://images1.kabum.com.br/produtos/fotos/sync_mirakl/632571/xlarge/SSD-500gb-M-2-Nvme-PCie-4-5000mb-s-Leit-3000mb-s-Grav-Nv3-Snv3s-500g-Kingston_1768698525.jpg" },
+        { name: "SSD Adata SU630, SATA, 480GB,", price:  429,image: "https://images6.kabum.com.br/produtos/fotos/100016/ssd-adata-su630-480gb-sata-iii-leitura-520mb-s-gravacao-450mb-s-asu630ss-480gq-r_1548783987_gg.jpg" },
+        ],
+    powerSupply: [
+        { name: "Fonte MSI MAG A650BN, 650W, 80 Plus Bronze", price:  349,image: "https://images8.kabum.com.br/produtos/fotos/369658/fonte-msi-mag-a650bn-atx-650w-80-plus-bronze-pfc-ativo-entrada-bivolt-preto-306-7zp2b22-ce0_1665770996_gg.jpg" },
+        { name: "Fonte Gamer Rise Mode Elektra, 650W", price:  279,image: "https://images1.kabum.com.br/produtos/fotos/725571/fonte-gamer-rise-mode-elektra-650w-bronze-full-modular-pfc-ativo-preto-rm-psu-elkm-650-b_1764181978_gg.jpg" },
+        { name: "Fonte XPG Core Reactor II VE, 850", price:  589,image: "https://images6.kabum.com.br/produtos/fotos/514896/fonte-xpg-core-reactor-ii-ve-850w-75261436_1721238617_gg.jpg" },
+        { name: "Fonte Gamer Rise Mode Zeus, 500W", price:  229,image: "https://images2.kabum.com.br/produtos/fotos/461382/fonte-gamer-rise-mode-zeus-500w-80-plus-white-pfc-ativo-preto-rm-psu-01-wt-500_1727696736_gg.jpg" },
+        { name: "Fonte Husky Sledger 650W, 80 Plus Bronze", price:  279,image: "https://images3.kabum.com.br/produtos/fotos/603223/fonte-husky-sledger-650w-80-plus-bronze-cybenetics-bronze-pfc-ativo-bivolt-hfn650pt_1765564037_gg.jpg" },
+        { name: "Fonte Corsair CX Series CX650, 650W", price:  399,image: "https://images6.kabum.com.br/produtos/fotos/516056/fonte-corsair-cx-series-cx650-650w-80-plus-bronze-sem-cabo-preto-cp-9020278-br_1714483460_gg.jpg" },
+        { name: "Fonte Gigabyte P650G PG5, 650W", price:  299,image: "https://images0.kabum.com.br/produtos/fotos/907600/fonte-gigabyte-p650g-pg5-650w-80-plus-gold-pfc-ativo-preto-28200-p65g5-1cbrr_1767367149_gg.jpg" },
+    ],
 
 }
 
@@ -129,6 +172,26 @@ function openModal(tipo) {
         case "Placa de Vídeo":
             components.gpu.forEach(el => {
                 createComponentOption(el, "Placa de Vídeo", "videoBoard");
+            })
+            break
+        case "Placa Mãe":
+            components.motherBoard.forEach(el => {
+                createComponentOption(el, "Placa Mãe", "motherBoard");
+            })
+            break
+        case "Memória Ram":
+            components.ram.forEach(el => {
+                createComponentOption(el, "Memória Ram", "ram");
+            })
+            break
+        case "Armazenamento":
+            components.ssd.forEach(el => {
+                createComponentOption(el, "Armazenamento", "storage");
+            })
+            break
+        case "Fonte":
+            components.powerSupply.forEach(el => {
+                createComponentOption(el, "Fonte", "powerSupply");
             })
             break
 
